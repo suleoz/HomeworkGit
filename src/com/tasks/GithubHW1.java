a3a0a35f57969c34dcee2fa8959c5b51605b9d07
suleoz <suleozdemir107@gmail.com>
2021-08-13 20:43:41 -0400

    first commit

---
+++ src/com/tasks/GithubHW1.java
@@ -0,0 +1,16 @@
+package com.tasks;
+
+public class GithubHW1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for(int i=1;i<11;i++) {
+			
+			System.out.print(i+" ");
+		}
+			
+	}
+
+}
